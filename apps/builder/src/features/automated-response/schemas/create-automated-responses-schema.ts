0e07ec5a02001657887675c2a427dd351b6a6ab2
realcodesiman <realcodesiman@gmail.com>
2025-03-05 02:12:45 +0700

    re-layout custom field

--- apps/builder/src/features/automated-response/schemas/create-automated-responses-schema.ts
+++ apps/builder/src/features/automated-response/schemas/create-automated-responses-schema.ts
@@ -8,24 +8,26 @@ export enum ReplyType {
 export const createAutomatedResponseRequest = z.object({
   folderId: z.union([z.string().cuid2(), z.null()]).default(null),
   userMessages: z.array(z.string().min(1).max(255)),
-  replies: z.array(
-    z.discriminatedUnion("type", [
-      z.object({
-        type: z.literal(ReplyType.FLOW),
-        flowId: z.string(),
-      }),
-      z.object({
-        type: z.literal(ReplyType.MESSAGE),
-        message: z.string().min(1).max(255),
-        buttons: z.array(
-          z.object({
-            label: z.string().min(1).max(255),
-            url: z.string().url(),
-          }),
-        ),
-      }),
-    ]),
-  ),
+  replies: z
+    .array(
+      z.discriminatedUnion("type", [
+        z.object({
+          type: z.literal(ReplyType.FLOW),
+          flowId: z.string(),
+        }),
+        z.object({
+          type: z.literal(ReplyType.MESSAGE),
+          message: z.string().min(1).max(255),
+          buttons: z.array(
+            z.object({
+              label: z.string().min(1).max(255),
+              url: z.string().url(),
+            }),
+          ),
+        }),
+      ]),
+    )
+    .min(1),
 })
 export type CreateAutomatedResponseRequest = z.infer<
   typeof createAutomatedResponseRequest
