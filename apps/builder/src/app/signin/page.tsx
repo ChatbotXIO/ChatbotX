5a57ce7ae5a34ee2ea313a97d1080d339f24c912
Real Codesiman <realcodesiman@gmail.com>
2025-11-21 06:08:06 +0700

    fix some bugs

--- apps/builder/src/app/signin/page.tsx
+++ apps/builder/src/app/signin/page.tsx
@@ -1,14 +1,6 @@
 import { SignInForm } from "@/features/auth/signin-form"
-import { auth } from "@/lib/auth/auth"
 
 export default async function SignInPage() {
-  await auth.api.signUpEmail({
-    body: {
-      name: "Demo", // required
-      email: "demo2@example.com", // required
-      password: "Demo@1234", // required
-    },
-  })
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
