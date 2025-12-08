74755f78d5412b9bbc6cf750665c04753954de6e
Real Codesiman <realcodesiman@gmail.com>
2025-12-08 08:34:34 +0700

    refator: use custom fields

--- apps/builder/src/lib/safe-action.ts
+++ apps/builder/src/lib/safe-action.ts
@@ -10,7 +10,6 @@ import { BaseException } from "./errors/exception"
 
 export const actionClient = createSafeActionClient({
   handleServerError(error) {
-    console.error("Error ->", error)
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       if (error.code === "P2002") {
         return `Unique constraint failed on ${error.meta?.target ?? ""}`
