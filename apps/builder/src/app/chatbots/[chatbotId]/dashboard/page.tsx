1d8b3bf311857edfe8a3df38014e5def4457d7c2
Deathgiver <anonymous@users.noreply.github.com>
2026-01-13 16:46:58 +0700

    Add filter

--- apps/builder/src/app/chatbots/[chatbotId]/dashboard/page.tsx
+++ apps/builder/src/app/chatbots/[chatbotId]/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import AnalysisFilterForm from "@/features/analysis/filter-form"
+import { AnalysisStoreProvider } from "@/features/analysis/provider/analysis-store-context"
+import NewContacts from "@/features/contacts/components/new-contact"
+import TotalContacts from "@/features/contacts/components/total-contacts"
 import { InboxCardList } from "@/features/inboxes/components/inbox-card-list"
 import InboxStatsList from "@/features/inboxes/components/inbox-stats-list"
 import { listInboxes } from "@/features/inboxes/queries"
-import { maxPerPage } from "@/lib/shared-request"
 
 export default async function Dashboard({
   params,
@@ -9,17 +12,25 @@ export default async function Dashboard({
   params: Promise<{ chatbotId: string }>
 }) {
   const { chatbotId } = await params
+
   const { data: inboxes } = await listInboxes({
     chatbotId,
     includes: ["integration"],
-    perPage: maxPerPage,
   })
 
   return (
     <div className="flex flex-col gap-4">
       <InboxCardList chatbotId={chatbotId} inboxes={inboxes} />
 
-      <InboxStatsList />
+      <AnalysisStoreProvider chatbotId={chatbotId}>
+        <AnalysisFilterForm />
+        <InboxStatsList />
+
+        <div className="grid grid-cols-2 gap-4">
+          <TotalContacts />
+          <NewContacts />
+        </div>
+      </AnalysisStoreProvider>
     </div>
   )
 }
