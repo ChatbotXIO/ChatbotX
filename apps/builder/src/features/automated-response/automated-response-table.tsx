64798a75b2e6aa842f6cb4f70d63765ccf34a583
Deathgiver <anonymous@users.noreply.github.com>
2026-03-23 22:15:18 +0700

    clean code

--- apps/builder/src/features/automated-response/automated-response-table.tsx
+++ apps/builder/src/features/automated-response/automated-response-table.tsx
@@ -251,7 +251,7 @@ export function AutomatedResponsesTable({
         enableHiding: false,
       },
     ],
-    [chatbotId, t, allFlows, searchParams.toString, searchParams],
+    [chatbotId, t, allFlows, searchParams],
   )
 
   const { table } = useDataTable({
