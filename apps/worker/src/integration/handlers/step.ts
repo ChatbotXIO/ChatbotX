999cf3496d7dd9a97a9a9c36a822ecd3a60d4d30
Hung Phan Viet <hungphan@ahachat.com>
2026-05-02 09:10:27 +0700

    feat: whatsapp option list

--- apps/worker/src/integration/handlers/step.ts
+++ apps/worker/src/integration/handlers/step.ts
@@ -367,5 +367,5 @@ export const flowStepHandlers: Record<
   [stepTypes.enum.email]: sendEmail,
   [stepTypes.enum.typing]: stepSendTyping,
   [stepTypes.enum.sendWaTemplateMessage]: sendFlowMessage,
-  [stepTypes.enum.whatsappOptionList]: undefined,
+  [stepTypes.enum.whatsappOptionList]: sendFlowMessage,
 }
