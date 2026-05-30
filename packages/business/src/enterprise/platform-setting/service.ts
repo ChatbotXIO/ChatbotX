78b1118e0bc32c7635991a0ea669575ce9b35db0
Real Codesiman <realcodesiman@gmail.com>
2026-05-30 16:18:26 +0700

    feat: correct checkout flow

--- packages/business/src/enterprise/platform-setting/service.ts
+++ packages/business/src/enterprise/platform-setting/service.ts
@@ -1,6 +1,6 @@
 import { db } from "@chatbotx.io/database/client"
 import { platformSettingModel } from "@chatbotx.io/database/schema"
-import { invalidateCacheByTags } from "@chatbotx.io/redis"
+import { invalidateCacheByTags, withCache } from "@chatbotx.io/redis"
 import type { EmailTemplate } from "../../platform/settings"
 
 type PlatformSettingUpsertData = {
@@ -22,14 +22,14 @@ type PlatformSettingUpsertData = {
 
 export const platformSettingService = {
   findForUser(userId: string) {
-    // return withCache(
-    //   `ps:${userId}`,
-    //   () =>
-    return db.query.platformSettingModel.findFirst({
-      where: { userId },
-    })
-    //   { tags: [`ps:${userId}`] },
-    // )
+    return withCache(
+      `ps:${userId}`,
+      () =>
+        db.query.platformSettingModel.findFirst({
+          where: { userId },
+        }),
+      { tags: [`ps:${userId}`] },
+    )
   },
 
   async upsert(userId: string, data: PlatformSettingUpsertData) {
