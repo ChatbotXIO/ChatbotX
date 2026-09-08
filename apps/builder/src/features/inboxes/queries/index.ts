b6ed384bee8fdc36a788e79be484eba903f3e773
Phan Viet Hung <viethung090@gmail.com>
2026-09-08 22:38:56 +0700

    revert(analytics): hide disconnected inboxes on the dashboard cards again (#1117)

--- apps/builder/src/features/inboxes/queries/index.ts
+++ apps/builder/src/features/inboxes/queries/index.ts
@@ -1,8 +1,8 @@
 import type { ListInboxesResponse } from "@chatbotx.io/business"
-import { inboxService, type ListInboxesInput } from "@chatbotx.io/business"
+import { inboxService, type ListInboxesRequest } from "@chatbotx.io/business"
 
 export async function listInboxes(
-  input: ListInboxesInput,
+  input: ListInboxesRequest,
 ): Promise<ListInboxesResponse> {
   return await inboxService.list(input)
 }
